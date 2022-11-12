866ee3082ad5fa709a8593af0859e71bd73cad15
Oleksii Vaskevych <alexeyffi@ukr.net>
2022-11-12 23:50:48 +0200

    add task02

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,29 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+const KEY = 'videoplayer-current-time';
+
+const onPlay = function (data) {
+  localStorage.setItem(KEY, data.seconds);
+};
+
+player.on('timeupdate', throttle(onPlay, 250));
+
+const currentTime = localStorage.getItem(KEY);
+
+if (currentTime) {
+  player
+    .setCurrentTime(currentTime)
+    .then(function (seconds) {})
+    .catch(function (error) {
+      switch (error.name) {
+        case 'RangeError':
+          break;
+
+        default:
+          break;
+      }
+    });
+}
